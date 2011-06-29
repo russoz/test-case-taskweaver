3a9e29d068e063ac276ec0b05b156e5843170c49
Alexei Znamensky <russoz@cpan.org>
2011-06-29 15:30:38 -0300

    first commit
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ My-Module/lib/My/Module.pm
@@ -0,0 +1,22 @@
+package My::Module;
+
+use strict;
+use warnings;
+
+# ABSTRACT: some abstract here
+
+# VERSION
+
+
+1;
+
+=pod
+
+=pkgroup A Group
+
+=pkg DataFlow 1.111620 OMG this version number!
+
+=pkg Moose 2.0010 some reason
+
+=cut
+
